a05819dae7e9d742046befaf386fbe036568ae3d
Vatsal Dave <ac121655@avcol.school.nz>
2025-02-26 09:40:32 +1300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodWasteManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
